198b45a0d2644b4067475bfd8ce8e62b6f5e6308
geekysid <siddhant.shah.1986@gmail.com>
2022-06-21 02:38:59 +0530

    ADD: Smart Contract Completed

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,71 +1,130 @@
 // SPDX-License-Identifier: MIT
 
+/**************************************
+    @title Vending Machine Silmuator
+    @author Siddhant Shah
+    @date June 20 2022
+***************************************/
+
 pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract VendingMachine {
 
+    // Product Object
     struct Product {
-        string productName;
-        uint256 productID;
-        uint256 price;
-        uint256 balance;
-        uint256 capacity;
-        StockStatus stockStatus;
-        ProductStatus productStatus;
+        string productName;                 // name of product
+        uint256 productID;                  // ID of product
+        uint256 price;                      // price of product
+        uint256 balance;                    // quantity remaining in vending machine
+        uint256 capacity;                   // maximum capacity of vending machine for this product
+        StockStatus stockStatus;            // status of product's stock in vending machine
+        ProductStatus productStatus;        // product's availability status
     }
 
-    enum StockStatus { OVERFLOW, FULL, OK, LOW, OUT_OF_STOCK }
-    enum ProductStatus { NOT_EXIST, ACTIVE, INACTIVE }
-    mapping(uint256 => Product) product;
-    uint256[] product_list;
-    address immutable owner;
+    enum StockStatus { OVERFLOW, FULL, OK, LOW, OUT_OF_STOCK }      // product's stock option
+    enum ProductStatus { NOT_EXIST, ACTIVE, INACTIVE }              // product's availability option
+    mapping(uint256 => Product) product;                // mapping of product's id to product itself
+    uint256[] productIDs;                               // list of all product's IDs
+    address immutable owner;                            // owner of vending machine
 
     constructor () {
         owner = msg.sender;
     }
 
-    event ProductAdded (uint256 productID, string productName, uint256 balance, uint256 price, StockStatus currentStockStatus);
-    event ProductUpdated (uint256 productID, uint256 capacity, uint256 price, StockStatus currentStockStatus);
-    event ProductReStocked (uint256 productID, uint256 quantityAdded, uint256 newBalance, StockStatus currentStockStatus);
-    event WithdrawSuccessfull(address account, uint256 _amount);
+    // EVENT Definitions
+    event ProductAdded (
+        uint256 productID,
+        string productName,
+        uint256 balance,
+        uint256 price,
+        StockStatus currentStockStatus
+    );              // event to to emited when owner successfully add new product to vending
 
-    // Modifier: making sure only Owner has acces
+    event ProductUpdated (
+        uint256 productID,
+        uint256 capacity,
+        uint256 price,
+        StockStatus currentStockStatus
+    );              // event to to emited when owner successfully updates a product's data
+
+    event ProductReStocked (
+        uint256 productID,
+        uint256 quantityAdded,
+        uint256 newBalance,
+        StockStatus currentStockStatus
+    );              // event to to emited when owner successfully add more quantity of a product
+
+    event WithdrawSuccessfull(
+        address account,
+        uint256 amount
+    );              // event to to emited when owner successfully withdraws from contract
+
+    event Purchased(
+        uint256 product_id,
+        uint256 quantity
+    );              // event to to emited when someone successfully purchases from vending machine
+
+    /* @title MODIFIER to ensure only Owner has acces */
     modifier onlyOwner {
         require(msg.sender == owner, "Only owner can access this functionality");
         _;
     }
 
-    // Modifier: making sure that product is not part of product_list
+    /* @title MODIFIER to ensure that product is not part of productIDs */
     modifier productNotExists(string memory _productName) {
         require(!productExists__name(_productName), "Product already exist by this name");
         _;
     }
 
-    // Modifier: making sure product exists
+    /* @title MODIFIER to ensure product exists */
     modifier productExists(uint256 _productID){
-        require(product[_productID].productStatus != ProductStatus.NOT_EXIST, "Product doesnot exists");
+        require(
+            product[_productID].productStatus != ProductStatus.NOT_EXIST,
+            "Product doesnot exists");
         _;
     }
 
-    // Modifier: making sure that product is available
+    /* @title MODIFIER to ensure that product is available */
+    modifier productIsActive(uint256 _productID) {
+        require(
+            product[_productID].productStatus == ProductStatus.ACTIVE,
+            "Owner has deactivate sale of this product. Please try another one"
+        );
+        _;
+    }
+
+    /* @title MODIFIER to ensure that product is available */
     modifier productIsInStock(uint256 _productID, uint256 _quantityRequired) {
-        require(product[_productID].stockStatus != StockStatus.OUT_OF_STOCK, "Product is out of stock");
+        require(
+            product[_productID].stockStatus != StockStatus.OUT_OF_STOCK,
+            "Product is out of stock"
+        );
         _;
     }
 
-    // Modifier: making sure that product is available
+    /* @title MODIFIER to ensure that product's minimum quantity is available */
     modifier productQuantityIsAvailable(uint256 _productID, uint256 _quantityRequired) {
-        require(product[_productID].balance >= _quantityRequired, string.concat("Product is a bit low in stock. Only ", Strings.toString(product[_productID].balance), " is avalable."));
+        require(
+            product[_productID].balance >= _quantityRequired,
+            string.concat("Product is a bit low in stock. Only ",
+                Strings.toString(product[_productID].balance),
+                " is avalable."
+            )
+        );
         _;
     }
 
-    // UTILITY Function to check if product exist with given name
+    /**
+        * @dev UTILITY FUNCTION to check if product exist with given name
+        * @param _productName String:  Name of product
+        * @return bool
+    */
     function productExists__name(string memory _productName) internal view returns (bool){
         bool _productExists;
-        for( uint256 i=0; i<product_list.length; i++ ){
-            if (keccak256(abi.encodePacked(product[product_list[i]].productName)) == keccak256(abi.encodePacked(_productName))) {
+        for( uint256 i=0; i<productIDs.length; i++ ){
+            if (keccak256(abi.encodePacked(product[productIDs[i]].productName)) == keccak256(abi.encodePacked(_productName))) {
                 _productExists = true;
                 break;
             }
@@ -73,8 +132,14 @@ contract VendingMachine {
         return _productExists;
     }
 
-    // UTILITY function to check status of stock
-    function getStockStatus(uint256 _currentStock, uint256 _capacity) internal pure returns (StockStatus currentStockStatus) {
+    /**
+        * @dev UTILITY FUNCTION to check status of stock
+        * @param _currentStock uint256 current stock of product
+        * @param _capacity uint256 maximum capacity of stock of product
+        * @return currentStockStatus StockStatus status of stock
+    */
+    function getStockStatus(uint256 _currentStock, uint256 _capacity) internal pure
+            returns (StockStatus currentStockStatus) {
         if (_currentStock == _capacity) {
             currentStockStatus = StockStatus.FULL;
         } else if (_currentStock > _capacity) {
@@ -88,11 +153,18 @@ contract VendingMachine {
         }
     }
 
-    // Function to allow owner to ad new products to the machine
-    function addNewProduct(string memory _name, uint256 _price, uint256 _balance, uint256 _capacity) external onlyOwner productNotExists(_name) {
-        uint256 _productID = product_list.length;
-        product_list.push(_productID);
-        Product storage _product = product[_productID];
+    /**
+        * @dev FUNCTION to allow owner to ad new products to the machine
+        * @param _name string name of product
+        * @param _price uint256 price of product
+        * @param _balance uint256 current stock of product
+        * @param _capacity uint256 maximum capacity of stock of product
+    */
+    function addNewProduct(string memory _name, uint256 _price, uint256 _balance, uint256 _capacity)
+            external onlyOwner productNotExists(_name) {
+        uint256 _productID = productIDs.length;                 // get a new productID
+        productIDs.push(_productID);                            // add productID to list of array that contains all array
+        Product storage _product = product[_productID];         // get a new product
         _product.productName = _name;
         _product.productID = _productID;
         _product.price = _price;
@@ -100,66 +172,162 @@ contract VendingMachine {
         _product.capacity = _capacity;
         _product.productStatus = ProductStatus.ACTIVE;
         _product.stockStatus = getStockStatus(_balance, _capacity);
-        emit ProductAdded(_product.productID, _product.productName , _product.balance, _product.price , product[_productID].stockStatus);
+
+        // emit an event
+        emit ProductAdded(
+            _product.productID,
+            _product.productName ,
+            _product.balance,
+            _product.price ,
+            product[_productID].stockStatus
+        );
     }
 
-    // Function to allow owner to ad new products to the machine
+    /**
+        * @dev FUNCTION to allow owner to ad new products to the machine
+        * @param _productID uint256 ID of product
+        * @param _price uint256 new price of product
+        * @param _capacity uint256 new maximum capacity of stock of product
+    */
     function editProduct(uint256 _productID, uint256 _price, uint256 _capacity) external onlyOwner productExists(_productID) {
-        Product storage _product = product[_productID];
+        Product storage _product = product[_productID];         // get product which needs to be updated
         _product.price = _price;
+
+        //  if capacity is changed that only go ahead
         if (_capacity != _product.capacity){
             _product.capacity = _capacity;
             _product.stockStatus = getStockStatus(_product.balance, _capacity);
         }
-        emit ProductUpdated(_product.productID, _product.balance, _product.price , product[_productID].stockStatus);
+
+        // emit event
+        emit ProductUpdated(
+            _product.productID,
+            _product.balance,
+            _product.price ,
+            product[_productID].stockStatus
+        );
     }
 
-    // Function to allow owner to restock a given products
+    /**
+        * @dev FUNCTION to allow owner to restock a given products
+        * @param _productID uint256 ID of product
+        * @param _quantityToAdd uint256 total quantity that needsto be added to stock
+    */
     function reStock(uint256 _productID, uint256 _quantityToAdd) external onlyOwner productExists(_productID) {
-        // making sure we dont over stock a product
+        // ensure we dont over stock a product
         if (product[_productID].balance + _quantityToAdd <= product[_productID].capacity) {
             product[_productID].balance += _quantityToAdd;          // adding new stock to old stock
-            product[_productID].stockStatus = getStockStatus(product[_productID].balance, product[_productID].capacity);        // updating stoock status
-            emit ProductReStocked(_productID, _quantityToAdd, product[_productID].balance, product[_productID].stockStatus);
+            product[_productID].stockStatus = getStockStatus(
+                                                product[_productID].balance,
+                                                product[_productID].capacity
+                                              );        // updating stoock status
+            // emiting event
+            emit ProductReStocked(
+                _productID,
+                _quantityToAdd,
+                product[_productID].balance,
+                product[_productID].stockStatus
+            );
         } else {
-            revert(string.concat("You can't add more than its capacity. Max allowed to add: ", Strings.toString(product[_productID].capacity-product[_productID].balance)));
+            revert(
+                string.concat("You can't add more than its capacity. Max allowed to add: ",
+                Strings.toString(product[_productID].capacity-product[_productID].balance))
+            );
         }
     }
 
-    // Function to allow owner to ad new products to the machine
+    /**
+        * @dev FUNCTION to allow owner to update existing product's status to the machine
+        * @param _productID uint256 ID of product
+        * @param _productStatus ProductStatus total quantity that needsto be added to stock
+    */
     function updateProductStatus(uint256 _productID, ProductStatus _productStatus) external onlyOwner productExists(_productID) {
-        Product storage _product = product[_productID];
-        _product.productStatus = _productStatus;
-        emit ProductAdded(_product.productID, _product.productName , _product.balance, _product.price , product[_productID].stockStatus);
+        Product storage _product = product[_productID];  // getting product thats needs to be updated
+        _product.productStatus = _productStatus;         // setting status
+
+        // emiting event
+        emit ProductAdded(
+            _product.productID,
+            _product.productName ,
+            _product.balance,
+            _product.price ,
+            product[_productID].stockStatus
+        );
     }
 
-    // Funtion to allow owner to withdraw funds
+    /**
+        * @dev FUNCTION to allow owner to withdraw funds
+        * @param _amount uint256 amount that owner wishes to withdraw
+    */
     function withdraw(uint256 _amount) external onlyOwner  {
         require(address(this).balance >= (_amount * 1 ether), "Not enough balance to withdraw");
-        (bool _success, ) = msg.sender.call{value: _amount * 1 ether}("");
+        (bool _success, ) = payable(msg.sender).call{value: _amount * 1 ether}("");     // transfering funds
+
+        //  if transfer succeed
         if (_success) {
-            emit WithdrawSuccessfull(msg.sender, _amount);
+            emit WithdrawSuccessfull(msg.sender, _amount);      // emit event
         } else{
             revert ("Unable to sent transaction because of some reason.");
         }
     }
 
+    /**
+        * @dev FUNCTION to allow user to buy a product
+        * @param _productID uint256 ID of product
+        * @param _quantity uint256 quantity purchased
+    */
+    function purchase(uint256 _productID, uint256 _quantity) payable external productExists(_productID)
+        productIsActive(_productID) productIsInStock(_productID, _quantity) productQuantityIsAvailable(_productID, _quantity) 
+    {
+        uint256 _totalPrice = product[_productID].price * _quantity * 1 ether;      // total price of transaction
+        require(
+            msg.value >= _totalPrice,
+            string.concat("Need a minmum of ", Strings.toString(_totalPrice), "WEI to purchase.")
+        );
+        product[_productID].balance -= _quantity;           // reduce the quantiity
+        product[_productID].stockStatus = getStockStatus(
+                                            product[_productID].balance,
+                                            product[_productID].capacity
+                                        );                  // updating stock status
+        // emiting event
+        emit Purchased(_productID, _quantity);
+    }
+
+    /**
+        * @dev FUNCTION to get product by ID
+        * @param _productID uint256 ID of product
+        * @return _product Product
+    */
     // Function to get product by ID
     function getProduct(uint256 _productID) external view productExists(_productID) returns (Product memory) {
         return product[_productID];
     }
 
-    // Function to get all products ID
+    /**
+        * @dev FUNCTION to get all products ID
+        * @return _products Product[] array of all products ID
+    */
     function getAllProductIDs() external view returns (uint256[] memory) {
-        return product_list;
+        return productIDs;
     }
 
-    // Function to get all products
+    /**
+        * @dev FUNCTION to get all products
+        * @return _products Product[] array of all products
+    */
     function getAllProductIDToProductName() external view returns (Product[] memory) {
-        Product[] memory _products = new Product[](product_list.length);
-        for (uint256 i; i<product_list.length; i++) {
-            _products[i] = product[product_list[i]];
+        Product[] memory _products = new Product[](productIDs.length);
+            for (uint256 i; i<productIDs.length; i++) {
+            _products[i] = product[productIDs[i]];
         }
         return _products;
+    }
+
+    /**
+        * @dev FUNCTION to allow owner to get balance of contract
+        * @return _balance uint256 balance of contract
+    */
+    function getContractBalance() external onlyOwner view returns (uint256) {
+        return address(this).balance / 1 ether;
     }
 }
