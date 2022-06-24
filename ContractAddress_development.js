847cae4478fa30da45a4d120ca1116f0a2e6dd14
geekysid <siddhant.shah.1986@gmail.com>
2022-06-24 17:21:24 +0530

    Deployed Contract to Ganache

---
+++ ContractAddress_development.js
@@ -0,0 +1,3 @@
+export const VendingMachineContractAddress = "0x948e559120d4B3e30B3F9EA371e0a44808c7550a";
+export const OwnerAddress = "0xbB216f78A2ce769385574E68170CC4436E569254";
+export const Network = "development";
--- migrations/2_vending_machine_migration.js
+++ migrations/2_vending_machine_migration.js
@@ -4,7 +4,7 @@ const VendingMachine = artifacts.require("VendingMachine");
 
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(VendingMachine);
-  fs.writeFileSync(`./ContractAddress-truffle--${network}.js`, `export const VendingMachineContractAddress = "${VendingMachine.address}";
+  fs.writeFileSync(`./ContractAddress_${network}.js`, `export const VendingMachineContractAddress = "${VendingMachine.address}";
 export const OwnerAddress = "${accounts[0]}";
 export const Network = "${network}";`);
 };
